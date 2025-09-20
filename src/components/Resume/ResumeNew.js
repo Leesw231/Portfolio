c070c10946b9b0e9a5109fb176b709a3f04b4027
Sewon231 <leesw1211@gmail.com>
2025-09-21 01:31:26 +0900

    .
    .

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -19,25 +19,28 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
+        {pdf && (
+          <Row className="resume">
+            <Document file={pdf} className="d-flex justify-content-center">
+              <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            </Document>
+          </Row>
+        )}
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
+        {pdf && (
+          <Row style={{ justifyContent: "center", position: "relative" }}>
+            <Button
+              variant="primary"
+              href={pdf}
+              target="_blank"
+              style={{ maxWidth: "250px" }}
+            >
+              <AiOutlineDownload />
+              &nbsp;Download CV
+            </Button>
+          </Row>
+        )}
       </Container>
     </div>
   );
