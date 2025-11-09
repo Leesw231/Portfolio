c664b041ec4e8cdaca408213c1be5ad7dfb7cefa
Sewon231 <leesw1211@gmail.com>
2025-11-10 04:54:17 +0900

    dk

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,16 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   let date = new Date();
-  let year = date.getFullYear();
+  let year = date.getFullYear(); // 'year' 변수 선언
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="6" className="footer-copywright">
           <h3>Designed and Developed by Sewon Lee</h3>
+        </Col>
+        <Col md="6" className="footer-copywright">
+          {/* --- 'year' 변수를 사용하도록 수정 --- */}
+          <h3>Copyright © {year} Sb</h3>
         </Col>
       </Row>
     </Container>
