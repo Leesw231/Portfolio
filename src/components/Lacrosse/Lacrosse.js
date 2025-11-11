1dcd700bc4a58002d041cd9c603ff94eb90cc7b4
Sewon231 <leesw1211@gmail.com>
2025-11-11 21:40:12 +0900

    dsjnj

--- src/components/Lacrosse/Lacrosse.js
+++ src/components/Lacrosse/Lacrosse.js
@@ -4,8 +4,8 @@ import Particle from "../Particle";
 import "./Lacrosse.css";
 
 // --- 비디오 파일 (이름을 직접 수정해주세요) ---
-import video1 from "../../Assets/Projects/Goal2025.MOV";
-import video2 from "../../Assets/Projects/Bodycheck2023.mov";
+import video1 from "../../Assets/Projects/Goal2025.mp4";
+import video2 from "../../Assets/Projects/Bodycheck2023.mp4";
 
 // --- 라크로스 사진들 ---
 import photo1 from "../../Assets/Projects/0I1A5148_Original.jpg";
@@ -48,13 +48,13 @@ function Lacrosse() {
         <Row style={{ justifyContent: "center", paddingBottom: "20px" }}>
           <Col md={6} className="video-col">
             <video controls className="Goal2025">
-              <source src={video1} type="Goal2025.MOV" />
+              <source src={video1} type="Goal2025.mp4" />
               Your browser does not support the video tag.
             </video>
           </Col>
           <Col md={6} className="video-col">
             <video controls className="Bodycheck2023">
-              <source src={video2} type="Bodycheck2023.mov" />
+              <source src={video2} type="Bodycheck2023.mp4" />
               Your browser does not support the video tag.
             </video>
           </Col>
